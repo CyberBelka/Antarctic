918eb6e3a7fb62f93b55888e79e3ce42411788c9
belka <vfrosenyuk@gmail.com>
2022-07-27 22:59:52 +0400

    На проверку

---
+++ source/js/modules/menu.js
@@ -0,0 +1,18 @@
+const initMenu = () => {
+  const nav = document.querySelector('.page-header__nav');
+  const toggle = document.querySelector('.page-header__toggle');
+
+  nav.classList.remove('page-header__nav--nojs');
+
+  toggle.addEventListener('click', function () {
+    if (nav.classList.contains('page-header__nav--closed')) {
+      nav.classList.remove('page-header__nav--closed');
+      nav.classList.add('page-header__nav--opened');
+    } else {
+      nav.classList.add('page-header__nav--closed');
+      nav.classList.remove('page-header__nav--opened');
+    }
+  });
+};
+
+export {initMenu};
